8103deff1a7e6ac4527f35246c8e7c0bbdf56231
RDestailleurs <romain.destailleurs1@gmail.com>
2025-08-01 23:01:18 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
-// ...existing code...
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur mon projet React + TypeScript !</h1>
+    </div>
+  );
+};
+
+export default App;
